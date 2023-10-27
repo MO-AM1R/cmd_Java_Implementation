0a6d901ccdf40b7558c9eb008bc38fcd236fa6fb
Mohamed Amir <me.mohamed.amir@gmail.com>
2023-10-27 11:38:14 +0200

    Add main file

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import Parser.Parser;
+import Terminal.Terminal;
+import java.util.Scanner;
+
+/**
+ *<pre>
+ *This class {@code Main} will handle the run
+ *of the application
+ *</pre>
+ * <blockquote>
+ * @version <strong style="color:'white'">1.0</strong>
+ * @author <pre style="color:'white'">Malik Khaled
+ *     Mohamed Amir
+ *     </pre>
+ * </blockquote>
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Parser parser = new Parser() ;
+        Terminal terminal = new Terminal() ;
+        String input = "";
+
+        while (!input.equals("exit")){
+            Scanner scanner = new Scanner(System.in) ;
+            System.out.print(terminal.getCurrentDirectory() + '>');
+            input = scanner.nextLine() ;
+            parser.parse(input) ;
+            terminal.setParser(parser);
+            terminal.chooseCommandAction();
+        }
+    }
+}
