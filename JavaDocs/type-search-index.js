6bc61f0df87ca6cbba155a9271ca853ee123cdcd
Mohamed Amir <me.mohamed.amir@gmail.com>
2023-11-03 21:38:39 +0200

    last commit -> push JavDoc

---
+++ JavaDocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"Parser","l":"Parser"},{"p":"Terminal","l":"Terminal"}];updateSearchResults();
